0de923d4450bb1542ee2deaa1172791f35f9bf7e
JaimeDdss <JaimeDdss>
2025-10-18 20:07:43 +0000

    creating bronze pipeline

---
+++ bronze_pipeline/transformations/bitcoin_bronze.sql
@@ -0,0 +1,13 @@
+-- Creating streaming table
+CREATE OR REFRESH STREAMING LIVE TABLE bitcoin
+TBLPROPERTIES ('quality' = 'bronze')
+AS
+-- Read JSON from volume and set map of options
+SELECT * 
+FROM cloud_files('/Volumes/lakehouse/raw_public/coinbase/coinbase/bitcoin_spot/'
+, 'json',
+ map(
+      'cloudFiles.inferColumnTypes', 'true',
+      'cloudFiles.includeExistingColumns', 'False',
+      'cloudFiles.schemaEvolutionMode','addNewColumns')
+);
