575e3953b8fa72c8ed802b7c60d7eabf25eb27cc
JaimeDdss <JaimeDdss>
2025-10-18 20:12:09 +0000

    update map func

--- bronze_pipeline/transformations/bitcoin_bronze.sql
+++ bronze_pipeline/transformations/bitcoin_bronze.sql
@@ -8,6 +8,6 @@ FROM cloud_files('/Volumes/lakehouse/raw_public/coinbase/coinbase/bitcoin_spot/'
 , 'json',
  map(
       'cloudFiles.inferColumnTypes', 'true',
-      'cloudFiles.includeExistingColumns', 'False',
+      'cloudFiles.includeExistingColumns', 'false',
       'cloudFiles.schemaEvolutionMode','addNewColumns')
 );
